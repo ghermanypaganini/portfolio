9e912e4bb216bb97298491e15b6905637dad4999
Ghermany <ghermanypaganini@gmail.com>
2024-03-06 23:54:50 -0300

    First commit

---
+++ components/about.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import React from "react";
+import SectionHeading from "./section-heading";
+import { motion } from "framer-motion";
+import { useSectionInView } from "@/lib/hooks";
+
+export default function About() {
+  const { ref } = useSectionInView("About");
+
+  return (
+    <motion.section
+      ref={ref}
+      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }}
+      id="about"
+    >
+      <SectionHeading>About me</SectionHeading>
+      <p>
+        I'm Ghermany Paganini, Brazilian from the state of Santa Catarina. I'm
+        an enthusiast of science and technology, love to travel, a bit of a
+        history buff and a sports fan.{" "}
+      </p>
+      <p>
+        I graduated in Computer Engineering and have been working in the IT
+        field for 7 years. Additionally, I have extensive experience in
+        hospitality.{" "}
+      </p>
+      <p>
+        {" "}
+        Currently, I'm studying web application development, cloud computing,
+        security and micro-SaaS business.
+      </p>
+      <p>
+        <span className="font-medium">
+          {" "}
+          Nowadays, I'm a backpacker traveling the world while working,
+          volunteering, learning new skills, getting to know cultures, places
+          and people!
+        </span>
+        <span className="italic"></span>
+      </p>
+    </motion.section>
+  );
+}
---
+++ lib/data.ts
@@ -0,0 +1,105 @@
+import React from "react";
+import { CgWorkAlt } from "react-icons/cg";
+import { FaReact } from "react-icons/fa";
+import { LuGraduationCap } from "react-icons/lu";
+import screentab from "@/public/Screen_tab.png";
+import rmtdevImg from "@/public/rmtdev.png";
+import n8n from "@/public/n8n.png";
+
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Experience",
+    hash: "#experience",
+  },
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
+
+export const experiencesData = [
+  {
+    title: "freelancer and backpacker",
+    location: "based in world",
+    description: "",
+    icon: React.createElement(LuGraduationCap),
+    date: "2024 - present",
+  },
+  {
+    title: "Jurerê In. Habitasul",
+    location: "Remote | Floripa, SC",
+    description: "systems support",
+    icon: React.createElement(CgWorkAlt),
+    date: "2021 - 2024",
+  },
+  {
+    title: "Irani SA",
+    location: "Joaçaba, SC",
+    description:
+      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
+    icon: React.createElement(FaReact),
+    date: "2017 - 2021",
+  },
+] as const;
+
+export const projectsData = [
+  {
+    title: "clone-tabnews",
+    description:
+      "The implementation of a forum project, aimed at development studies, inspired by Tabnews.com.br.",
+    tags: ["Javascript", "PostgreSQL", "Tailwind", "Vercel hosting"],
+    imageUrl: screentab,
+  },
+  {
+    title: "this portfolio",
+    description:
+      "Portfolio web page using modern dev frameworks. Code is available on GitHub.",
+    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Open Source"],
+    imageUrl: rmtdevImg,
+  },
+  {
+    title: "automation",
+    description: "Automation tasks and workflows.",
+    tags: ["N8N", "Integrations", "Webhooks"],
+    imageUrl: n8n,
+  },
+] as const;
+
+export const skillsData = [
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Next.js",
+  "Node.js",
+  "Git",
+  "Tailwind",
+  "Prisma",
+  "Redux",
+  "GraphQL",
+  "Apollo",
+  "PostgreSQL",
+  "Python",
+  "Django",
+  "Framer Motion",
+  "Linux",
+  "AWS",
+  "Wordpress",
+  "Scrum",
+  "PMS",
+] as const;
